c8427ee1385001f8e70b737cfc4caa42b64c7e30
hend37 <hendhossam36@gmail.com>
2023-10-01 12:56:22 +0300

    output,operation,fun,

---
+++ Hend.CTD.s_tasks/JS DEMOS/script.js
@@ -0,0 +1,42 @@
+//4 OUTPUT methods
+// document.getElementById("name").innerHTML = "Hend";
+// alert("Hi Hend");
+// documement.write("Hi There");
+// note writ make override 
+// بمعنى انها بتمسح القيمة القديمة فى الhtml
+//  وتضيف القيمة الجديدة فى ال js
+// console.log("Hanoda"); //not with write 
+
+//2-operations
+//Arithimatics + - * / % ** ++ --
+//Assignment opertator =
+
+// variables 
+//declare variables & assign it
+// let firstnumber = 3 , lastnumber= 5;
+// let result = firstnumber + lastnumber;
+// console.log(result);
+// 8 Data Type
+// 1-Numbers
+// let x-1;
+// // 2-string
+// let name='Hend Hossam';
+// // 3-array
+// const person ['Hend','Hossam','Ali','Maher']
+
+// Functions
+// function sum(){
+//     let firstnumber = 10 , lastnumber= 5;
+//  let result = firstnumber + lastnumber;
+// console.log(result);
+// }
+// // call function
+// sum();
+function sum(firstnumber,lastnumber){
+  let result = firstnumber + lastnumber;
+// console.log(result);
+return result; 
+}
+// call function
+// sum(15,10);
+console.log(sum(15,10));
