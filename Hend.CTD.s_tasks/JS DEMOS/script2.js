0074f24a04309ba4c473b2c4815f412eec9259f4
hend37 <hendhossam36@gmail.com>
2023-10-03 21:46:09 +0300

    var_object

---
+++ Hend.CTD.s_tasks/JS DEMOS/script2.js
@@ -0,0 +1,8 @@
+// Variable_object
+const car ={
+    model:"BMW",
+    color:"blue",
+     year:"2023"    //no comma the last proprty
+};
+const person ={name:"Hend",weight:100,age:25};
+console.log(person.name);
