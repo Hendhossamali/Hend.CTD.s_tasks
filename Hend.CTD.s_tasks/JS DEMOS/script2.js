b3390a629ccd41253fdc705798dd2c1a1c051f19
hend37 <hendhossam36@gmail.com>
2023-10-04 21:52:33 +0300

    new

--- Hend.CTD.s_tasks/JS DEMOS/script2.js
+++ Hend.CTD.s_tasks/JS DEMOS/script2.js
@@ -4,5 +4,27 @@ const car ={
     color:"blue",
      year:"2023"    //no comma the last proprty
 };
-const person ={name:"Hend",weight:100,age:25};
-console.log(person.name);
+// const person ={name:"Hend",weight:100,age:25};
+// accsess object property
+//  1-console.log(person.name);
+//  console.log(person["age"]); //2
+
+// console.log(person);
+
+// methods for objects(fun)
+const person ={
+    firstname:"Hend",
+    lastname:"Hossam",
+    weight:100,
+    age:25
+   fullName: function(){
+    return this.firstname + "" + this.lastname;
+   }
+};
+console.log(person.fullName());
+
+//comparison 
+//if ,if else, switch
+//loops, for ,for in , while 
+
+
